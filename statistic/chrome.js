7d2c0e461ff9e793d6ff45de148a60b827c65588
brian <briankadenge@gmail.com>
2026-02-10 13:57:09 +0300

    html

---
+++ statistic/chrome.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    document.getElementById(mybtn).style.backgroundColor="beige"
+}
+function test(){
+    alert("hello good morning")
+}
+function abcd(){
+    document.getElementById("mypara").style.color="purple"
+}
